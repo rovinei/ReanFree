b40219621ab6017abe6e0465f1240bc7807eec82
rovinei <ro.vinei@yahoo.com>
2017-06-17 12:25:03 +0700

    pagiante with orderby

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -66,7 +66,8 @@ class PageController extends Controller
         try{
             $articles = Post::where('mediatype_id', '=', 1)
                             ->where('category_id', '=', 1)
-                            ->paginate(16);
+                            ->paginate(16)
+                            ->orderBy('created_at', 'desc');
             $suggestArticles = Post::where('mediatype_id', '=', 1)
                                 ->where('created_at', '<=', Carbon::today())
                                 ->take(4)->get();
