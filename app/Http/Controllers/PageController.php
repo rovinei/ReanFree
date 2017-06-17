64cb9324f088d154f5ed4fb81d182a194c22dfde
rovinei <ro.vinei@yahoo.com>
2017-06-17 12:28:26 +0700

    correct orderby method

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -66,8 +66,8 @@ class PageController extends Controller
         try{
             $articles = Post::where('mediatype_id', '=', 1)
                             ->where('category_id', '=', 1)
-                            ->paginate(16)
-                            ->orderBy('created_at', 'desc');
+                            ->orderBy('created_at', 'desc')
+                            ->paginate(16);
             $suggestArticles = Post::where('mediatype_id', '=', 1)
                                 ->where('created_at', '<=', Carbon::today())
                                 ->take(4)->get();
